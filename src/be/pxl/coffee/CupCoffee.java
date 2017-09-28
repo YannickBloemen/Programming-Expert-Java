643a75941567211311904ed80b39f14a183194ed
YannickBloemen <yannick.bloemen@student.pxl.be>
2017-09-28 14:44:07 +0200

    init

---
+++ src/be/pxl/coffee/CupCoffee.java
@@ -0,0 +1,8 @@
+package be.pxl.coffee;
+
+/**
+ * Created by Yannick Bloemen on 28/09/2017.
+ */
+public class CupCoffee {
+}
+
---
+++ src/be/pxl/coffee/Laté.java
@@ -0,0 +1,7 @@
+package be.pxl.coffee;
+
+/**
+ * Created by Yannick Bloemen on 28/09/2017.
+ */
+public class Laté {
+}
